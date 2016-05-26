57b98c6e5b439d6e7b4470512aef5d375c1ae398
Evgeny Denisov <edenisov@dev.rtsoft.ru>
2016-05-26 18:27:06 +0400

    implement serial port open

--- src/target/linux/aj_target_serial.c
+++ src/target/linux/aj_target_serial.c
@@ -23,7 +23,12 @@
  */
 #define AJ_MODULE TARGET_SERIAL
 
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <signal.h>
 #include <sys/types.h>
+#include <sys/file.h>
 
 #include <ajtcl/aj_target.h>
 #include <ajtcl/aj_status.h>
@@ -33,7 +38,7 @@
 #include <ajtcl/aj_serial_tx.h>
 #include <ajtcl/aj_serio.h>
 
-#ifdef AJ_DEBUG_SERIAL_TARGET
+#ifdef AJ_DEBUG_TARGET_SERIAL
 #define AJ_DebugDumpSerialRX(a, b, c) AJ_DumpBytes(a, b, c)
 #define AJ_DebugDumpSerialTX(a, b, c) AJ_DumpBytes(a, b, c)
 #else
@@ -57,6 +62,9 @@ static AJ_SerIOTxCompleteFunc SendCB;
  */
 AJ_SerialTxFunc g_AJ_TX;
 
+/** Serial Port FD */
+static int serialFD = -1;
+
 static struct {
     uint8_t* data;
     uint32_t len;
@@ -71,7 +79,7 @@ AJ_Status AJ_Serial_Up()
 {
     AJ_Status status;
 
-    AJ_Printf("AJ_Serial_Up\n");
+    AJ_InfoPrintf(("AJ_Serial_Up\n"));
     
     return AJ_SerialInit(AJ_SERIAL_DEVICE, 115200, AJ_SERIAL_WINDOW_SIZE, AJ_SERIAL_PACKET_SIZE);
 }
@@ -80,11 +88,14 @@ AJ_Status AJ_SerialTargetInit(const char* ttyName, uint32_t bitRate)
 {
     AJ_SerIOConfig config;
 
-    AJ_Printf("AJ_SerialTargetInit %s\n", ttyName);
+    AJ_InfoPrintf(("AJ_SerialTargetInit %s\n", ttyName));
 
     config.bitrate = bitRate;
     config.config = (const void *)ttyName;
-    /* TODO: stop bits, etc... */
+    /* TODO: read from nvram, etc? */
+    config.bits = 8;
+    config.stopBits = 1;
+    config.parity = 0;
 
     return  AJ_SerialIOInit(&config);
 }
@@ -97,7 +108,7 @@ static void readBytesFromUart(uint8_t* data, uint32_t len)
     static uint8_t ReadingMsg = FALSE;
 
     // if there is data ready,
-    AJ_Printf("readBytesFromUart: %d\n", len);
+    AJ_InfoPrintf(("readBytesFromUart: %d\n", len));
     AJ_ASSERT(rx_buf.data != NULL);
     while (len) {
         if (rx_buf.pos >= rx_buf.data + rx_buf.len) {
@@ -133,7 +144,7 @@ static void readBytesFromUart(uint8_t* data, uint32_t len)
 /* This function sets up a buffer for us to fill with RX data */
 void AJ_RX(uint8_t* buf, uint32_t len)
 {
-    //AJ_Printf("AJ_RX: %d\n", len);
+    //AJ_InfoPrintf(("AJ_RX: %d\n", len));
     AJ_ASSERT(buf != NULL);
     rx_buf.data = buf;
     rx_buf.pos = buf;
@@ -155,7 +166,7 @@ static void runTx()
     uint32_t len = tx_buf.len - (tx_buf.pos - tx_buf.data);
 
     if (len) {
-        AJ_Printf("runTx: (%d)\n", len);
+        AJ_InfoPrintf(("runTx: (%d)\n", len));
     }
 
     if (!tx_buf.data || !tx_buf.pos || !tx_buf.len || !len /*|| !aci_state.data_credit_available*/) {
@@ -176,14 +187,14 @@ static void runTx()
             break;
         }
     }
-    AJ_Printf("Tx (credits: %d)\n", aci_state.data_credit_available);
+    AJ_InfoPrintf(("Tx (credits: %d)\n", aci_state.data_credit_available));
 */
 }
 
 /* This function is requesting us to send data over our UART */
 void __AJ_TX(uint8_t* buf, uint32_t len)
 {
-    AJ_Printf("__AJ_TX: %d\n", len);
+    AJ_InfoPrintf(("__AJ_TX: %d\n", len));
     tx_buf.data = buf;
     tx_buf.pos = buf;
     tx_buf.len = len;
@@ -193,7 +204,7 @@ void __AJ_TX(uint8_t* buf, uint32_t len)
 
 void AJ_TX(uint8_t* buf, uint32_t len)
 {
-    AJ_Printf("AJ_TX: %d\n", len);
+    AJ_InfoPrintf(("AJ_TX: %d\n", len));
     g_AJ_TX(buf, len); // should call the inner implementation
 }
 
@@ -210,7 +221,7 @@ void AJ_ResumeTX()
 
 AJ_Status AJ_SerialIOEnable(uint32_t direction, uint8_t enable)
 {
-    AJ_Printf("AJ_SerialIOEnable -->%d: %d\n", direction, enable);
+    AJ_InfoPrintf(("AJ_SerialIOEnable -->%d: %d\n", direction, enable));
     AJ_Status status = AJ_OK;
 
     if (direction == AJ_SERIO_RX) {
@@ -228,35 +239,204 @@ AJ_Status AJ_SerialIOEnable(uint32_t direction, uint8_t enable)
 
 void AJ_SetRxCB(AJ_SerIORxCompleteFunc rx_cb)
 {
-    AJ_Printf("AJ_SetRxCB\n");
+    AJ_InfoPrintf(("AJ_SetRxCB\n"));
     RecvCB = rx_cb;
 }
 
 void AJ_SetTxCB(AJ_SerIOTxCompleteFunc tx_cb)
 {
-    AJ_Printf("AJ_SetTxCB\n");
+    AJ_InfoPrintf(("AJ_SetTxCB\n"));
     SendCB = tx_cb;
 }
 
 void AJ_SetTxSerialTransmit(AJ_SerialTxFunc tx_func)
 {
-    AJ_Printf("AJ_SetTxSerialTransmit\n");
+    AJ_InfoPrintf(("AJ_SetTxSerialTransmit\n"));
     g_AJ_TX = tx_func;
 }
 
 
 AJ_Status AJ_SerialIOInit(AJ_SerIOConfig* config)
 {
-    AJ_Printf("AJ_SerialIOInit\n");
+    int ret;
+    struct termios ttySettings;
+    speed_t speed;
+
+    AJ_InfoPrintf(("AJ_SerialIOInit\n"));
+
+    serialFD = -1;
+
+    /*
+     * Validate and set parameters
+     */
+    memset(&ttySettings, 0, sizeof(ttySettings));
+    ttySettings.c_cflag |= CLOCAL | CREAD;
+
+    /*
+     * Set input and output baudrate
+     */
+    switch (config->bitrate) {
+    case 2400:
+        speed = B2400;
+        break;
+    case 9600:
+        speed = B9600;
+        break;
+    case 19200:
+        speed = B19200;
+        break;
+    case 38400:
+        speed = B38400;
+        break;
+    case 57600:
+        speed = B57600;
+        break;
+    case 115200:
+        speed = B115200;
+        break;
+    case 230400:
+        speed = B230400;
+        break;
+    case 460800:
+        speed = B460800;
+        break;
+    case 921600:
+        speed = B921600;
+        break;
+    case 1000000:
+        speed = B1000000;
+        break;
+    case 1152000:
+        speed = B1152000;
+        break;
+    case 1500000:
+        speed = B1500000;
+        break;
+    case 2000000:
+        speed = B2000000;
+        break;
+    case 2500000:
+        speed = B2500000;
+        break;
+    case 3000000:
+        speed = B3000000;
+        break;
+    case 3500000:
+        speed = B3500000;
+        break;
+    case 4000000:
+        speed = B4000000;
+        break;
+
+    default:
+        AJ_ErrPrintf(("Invalid bitrate %d\n", config->bitrate));
+        return AJ_ERR_INVALID;
+    }
+    cfsetospeed(&ttySettings, speed);
+    cfsetispeed(&ttySettings, speed);
+
+    switch (config->bits) {
+    case 5:
+        ttySettings.c_cflag |= CS5;
+        break;
+    case 6:
+        ttySettings.c_cflag |= CS6;
+        break;
+    case 7:
+        ttySettings.c_cflag |= CS7;
+        break;
+    case 8:
+        ttySettings.c_cflag |= CS8;
+        break;
+
+    default:
+        AJ_ErrPrintf(("Invalid databits %d\n", config->bits));
+        return AJ_ERR_INVALID;
+    }
+
+    switch (config->parity) {
+    case 0 /* 'n' */:
+        ttySettings.c_cflag &= ~(PARENB | PARODD);
+        break;
+    case 1 /* 'o' */:
+        ttySettings.c_iflag |= INPCK;
+        ttySettings.c_cflag |= PARENB | PARODD;
+        break;
+    case 2 /* 'e' */:
+        ttySettings.c_iflag |= INPCK;
+        ttySettings.c_cflag |= PARENB;
+        break;
+
+    default:
+        AJ_ErrPrintf(("Invalid parity %s\n", config->parity));
+        return AJ_ERR_INVALID;
+    }
+
+    switch (config->stopBits) {
+    case 1:
+        ttySettings.c_cflag &= ~CSTOPB;
+        break;
+    case 2:
+        ttySettings.c_cflag |= CSTOPB;
+        break;
+
+    default:
+        AJ_ErrPrintf(("Invalid Invalid stopbits %d\n", config->stopBits));
+        return AJ_ERR_INVALID;
+    }
+
+    ret = open((const char *)config->config, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (ret == -1) {
+        AJ_ErrPrintf(("failed to open serial device %s. ret = %d, %d - %s\n", (const char *)config->config, ret, errno, strerror(errno)));
+        goto error;
+    }
+    serialFD = ret;
+
+    /* Lock this FD, to ensure exclusive access to this serial port. */
+    ret = flock(serialFD, LOCK_EX | LOCK_NB);
+    if (ret) {
+        AJ_ErrPrintf(("Lock serialFD %d failed with '%s'\n", serialFD, strerror(errno)));
+        goto error;
+    }
+
+    AJ_InfoPrintf(("opened serial device %s successfully. ret = %d\n", (const char *)config->config, ret));
+
+    ret = tcflush(serialFD, TCIOFLUSH);
+    if (ret) {
+        AJ_ErrPrintf(("Flush serialFD %d failed with '%s'\n", serialFD, strerror(errno)));
+        goto error;
+    }
+
+    /**
+     * Set the new options on the port
+     */
+    ret = tcsetattr(serialFD, TCSANOW, &ttySettings);
+    if (ret) {
+        AJ_ErrPrintf(("Set parameters serialFD %d failed with '%s'\n", serialFD, strerror(errno)));
+        goto error;
+    }
+
+    ret = tcflush(serialFD, TCIOFLUSH);
+    if (ret) {
+        AJ_ErrPrintf(("Flush serialFD %d failed with '%s'\n", serialFD, strerror(errno)));
+        goto error;
+    }
 
 //    AJ_SetSioCheck(aci_loop);
-
     return AJ_OK;
+
+error:
+    if (serialFD != -1) {
+        close(serialFD);
+        serialFD = -1;
+    }
+    return AJ_ERR_DRIVER;
+
 }
 
 AJ_Status AJ_SerialIOShutdown(void)
 {
-    AJ_Printf("AJ_SerialIOShutdown\n");
+    AJ_InfoPrintf(("AJ_SerialIOShutdown\n"));
     return AJ_OK;
 }
 
